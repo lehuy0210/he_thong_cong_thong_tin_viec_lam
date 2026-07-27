63176d3604d6f0e835815471cd9c3574c2325edd
lehuy0210 <nhokhuypro78999@gmail.com>
2026-07-27 08:54:24 +0700

    cv-backend; Khoi tao file ket noi database

---
+++ cv-backend/db.py
@@ -0,0 +1,19 @@
+import os
+import mysql.connector
+from mysql.connector import Error
+
+# Khai báo db_config gom chung
+db_config = {
+    'host': os.getenv('DB_HOST', 'localhost'),
+    'user': os.getenv('DB_USER', 'root'),          
+    'password': os.getenv('DB_PASSWORD', 'lehuy0210'),          
+    'database': os.getenv('DB_NAME', 'hethongcv') 
+}
+
+def get_db_connection():
+    try:
+        conn = mysql.connector.connect(**db_config)
+        return conn
+    except Error as e:
+        print(f"Lỗi kết nối MySQL: {e}")
+        return None

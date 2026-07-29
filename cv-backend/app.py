204d3cbc58695e98cca71e225146a63fcba4f91b
nhoxdangerhjx222 <nhoxdangerhjx222@gmail.com>
2026-07-30 03:37:01 +0700

    add job posting management

--- cv-backend/app.py
+++ cv-backend/app.py
@@ -1,11 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask
 from flask_cors import CORS
 from routes.auth import auth_bp
+from routes.job_management import job_bp
 
 app = Flask(__name__)
 CORS(app)  # Cho phép tất cả các domain (bao gồm React localhost) gọi API
 
 app.register_blueprint(auth_bp, url_prefix='/api')
+app.register_blueprint(job_bp, url_prefix='/api')
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
